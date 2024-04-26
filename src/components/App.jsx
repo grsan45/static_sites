7f0cc859c82082d4c4ac3486e6e36754995f5b10
Grayson Amendt <grsan45@gmail.com>
2024-04-25 23:28:25 -0400

    switch to hash router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import {Route, HashRouter as Router, Routes} from 'react-router-dom'
 
 import Home from "../views/Home"
 import OGNameGen from "../views/OGNameGen"
