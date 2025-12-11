a938fa287b7d4199ea6a7cfbe346133f7036c0a1
Grayson Amendt <grsan45@gmail.com>
2025-12-11 02:56:41 -0500

    Remove test code

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,9 +7,6 @@ import Col from "../components/Col";
 const Home = () => {
     return (
          <View>
-             <Col>
-                 <h2>Test</h2>
-             </Col>
              <NavColumn title="Projects">
                  <Link className="main-nav-link" to="goose">GOOSE (Grayson's Odd Operating System Endeavour)</Link>
                  <Link className="main-nav-link" to="ntm">NoTrackingMarkers</Link>
